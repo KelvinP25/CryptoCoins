78c17e7f1e9ee9051521855ba00f49569e982e9e
Kelvin Peña <kelvincastillo9911@gmail.com>
2022-06-28 21:21:39 -0400

    Modificando el CoinListState

--- app/src/main/java/com/example/cryptocoins/data/remoto/dto/CoinListState.kt
+++ app/src/main/java/com/example/cryptocoins/data/remoto/dto/CoinListState.kt
@@ -2,6 +2,6 @@ package com.example.cryptocoins.data.remoto.dto
 
 data class CoinListState(
     val isLoading: Boolean = false,
-    val exchanges: List<Coin> = emptyList(),
+    val coins: List<Coin> = emptyList(),
     val error: String = ""
 )
