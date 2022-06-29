5f03e60553e2c89477e8d0721230b462f17ee30d
Kelvin Peña <kelvincastillo9911@gmail.com>
2022-06-28 20:42:06 -0400

    Creando el AppModule

---
+++ app/src/main/java/com/example/cryptocoins/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.example.cryptocoins.di
+
+import com.example.cryptocoins.data.remoto.CoinApi
+import com.example.cryptocoins.data.repository.CoinRepository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinApi(moshi: Moshi): CoinApi{
+        return Retrofit.Builder()
+            .baseUrl("https://localhost:7036")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(CoinApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(coinApi: CoinApi): CoinRepository{
+        return CoinRepository(coinApi)
+    }
+}
