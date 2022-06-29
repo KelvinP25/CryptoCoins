d95620cedf5ad202734f4218f7a2804796c01dd1
Kelvin Peña <kelvincastillo9911@gmail.com>
2022-06-28 21:03:30 -0400

    Creando el ViewModel de coin

---
+++ app/src/main/java/com/example/cryptocoins/ui/theme/Coin/CoinViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.cryptocoins.ui.theme.Coin
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptocoins.data.remoto.dto.CoinListState
+import com.example.cryptocoins.data.repository.CoinRepository
+import com.example.cryptocoins.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinViewModel @Inject constructor(
+    private val coinRepository: CoinRepository
+): ViewModel(){
+    private var _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        coinRepository.getCoins().onEach { result ->
+            when(result) {
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+                is Resource.Success -> {
+                    _state.value = CoinListState(exchanges = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = CoinListState(error = result.message ?: "Error desconocido")
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
