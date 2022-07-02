e43f645bcbb6ce82b5dda3a09cd638a04073416c
Kelvin Peña <kelvincastillo9911@gmail.com>
2022-07-02 17:44:35 -0400

    Modificando el data class de Coin

--- app/src/main/java/com/example/cryptocoins/data/remoto/dto/Coin.kt
+++ app/src/main/java/com/example/cryptocoins/data/remoto/dto/Coin.kt
@@ -1,8 +1,8 @@
 package com.example.cryptocoins.data.remoto.dto
 
 data class Coin(
-    val monedaId: Int = 0,
-    val descricion: String? = "",
-    val valor: Double = 0.0,
-    val imagenUrl : String = ""
+    val monedaId: String? = "",
+    val descripcion: String? = "",
+    val valor: Double? = 0.00,
+    val imageUrl : String? = ""
 )
