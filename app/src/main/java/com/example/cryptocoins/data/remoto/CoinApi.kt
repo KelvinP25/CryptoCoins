465c8d247b7b02fe3e83f9635d4fb9e4dc6558b1
Kelvin Peña <kelvincastillo9911@gmail.com>
2022-07-04 01:22:59 -0400

    Agregando Post al CoinApi

--- app/src/main/java/com/example/cryptocoins/data/remoto/CoinApi.kt
+++ app/src/main/java/com/example/cryptocoins/data/remoto/CoinApi.kt
@@ -1,9 +1,15 @@
 package com.example.cryptocoins.data.remoto
 
 import com.example.cryptocoins.data.remoto.dto.Coin
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface CoinApi {
     @GET("/Coinst")
     suspend fun getCoin(): List<Coin>
+
+    @POST("/Coinst")
+    suspend fun postCoin(@Body coin: Coin): Response<Coin>
 }
