c6d517e33a1f19a3c492aacdbfb3a34b71b4eaf5
Kelvin Peña <kelvincastillo9911@gmail.com>
2022-07-04 01:25:54 -0400

    Terminando registro de coins

--- app/src/main/java/com/example/cryptocoins/ui/theme/Coin/RegistroCoinScreen.kt
+++ app/src/main/java/com/example/cryptocoins/ui/theme/Coin/RegistroCoinScreen.kt
@@ -1,31 +1,41 @@
 package com.example.cryptocoins.ui.theme.Coin
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.example.cryptocoins.R
+import com.example.cryptocoins.util.Validar
 
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@SuppressLint("UnrememberedMutableState,UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun RegistroCoinScreen(navHostController: NavHostController) {
+fun RegistroCoinScreen(
+    navHostController: NavHostController,
+    viewModel: CoinViewModel = hiltViewModel()
+) {
 
+    var errordes by remember {
+        mutableStateOf(false)
+    }
+    var errorval by remember {
+        mutableStateOf(false)
+    }
+    val contexto = LocalContext.current
 
     Scaffold(
         topBar = {
@@ -42,7 +52,7 @@ fun RegistroCoinScreen(navHostController: NavHostController) {
                 navigationIcon = {
                     IconButton(
                         onClick = {
-                            navHostController.popBackStack()
+                            navHostController.navigate("CoinScreen")
                         }
                     ) {
                         Icon(
@@ -55,16 +65,14 @@ fun RegistroCoinScreen(navHostController: NavHostController) {
                 },
                 backgroundColor = MaterialTheme.colors.onSecondary,
             )
-        },
-
-
-        ) {
+        }
+    ) {
 
         Column(Modifier.padding(16.dp)) {
             Spacer(modifier = Modifier.height(20.dp))
             OutlinedTextField(
-                value = "",
-                onValueChange = { },
+                value = viewModel._description,
+                onValueChange = { viewModel._description = it },
                 modifier = Modifier.fillMaxWidth(),
                 label = { Text(text = "Moneda") },
                 leadingIcon = {
@@ -72,12 +80,15 @@ fun RegistroCoinScreen(navHostController: NavHostController) {
                         imageVector = Icons.Default.CurrencyBitcoin,
                         contentDescription = null
                     )
-                }
+                },
+                isError = errordes
+
             )
+            Validar(error = errordes, text = "Ingrese una Moneda")
             Spacer(modifier = Modifier.height(20.dp))
             OutlinedTextField(
-                value = "",
-                onValueChange = { },
+                value = viewModel._valor,
+                onValueChange = { viewModel._valor = it },
                 modifier = Modifier.fillMaxWidth(),
                 label = { Text(text = "Precio") },
                 leadingIcon = {
@@ -86,10 +97,49 @@ fun RegistroCoinScreen(navHostController: NavHostController) {
                         contentDescription = null
                     )
                 },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                isError = errorval
+            )
+            Validar(error = errorval, text = "Ingrese un Precio")
+            Spacer(modifier = Modifier.height(20.dp))
+            OutlinedTextField(
+                value = viewModel._image,
+                onValueChange = { viewModel._image = it },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "URL de Imagen") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Image,
+                        contentDescription = null
+                    )
+                }
             )
             Spacer(modifier = Modifier.height(28.dp))
-            Button(onClick = {},
+            Button(
+                onClick = {
+                    errordes = viewModel._description.isBlank()
+                    errorval = viewModel._valor.isBlank()
+                    if (!errordes && !errorval) {
+                        if (viewModel._valor.toDouble() > 0) {
+                            viewModel.Post()
+                            Toast.makeText(
+                                contexto,
+                                "La moneda se guardó correctamente",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            // navHostController.navigate("")
+                        } else {
+                            Toast.makeText(
+                                contexto,
+                                "El precio no puede ser menor o igual a cero",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                    viewModel._description = ""
+                    viewModel._valor = 0.00.toString()
+                    viewModel._image = ""
+                },
 
                 modifier = Modifier
                     .padding(16.dp)
@@ -101,7 +151,12 @@ fun RegistroCoinScreen(navHostController: NavHostController) {
                 ),
                 shape = CutCornerShape(6.dp),
 
-            ){
+                ) {
+                Icon(
+                    imageVector = Icons.Default.Save,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(20.dp))
                 Text(
                     text = "Guardar",
                     fontFamily = FontFamily.Default,
